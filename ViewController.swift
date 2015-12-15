2d554ed317b4a87b6b4d8d92b09ef1a4e0f950a3
denonbarton <dbar0540@740s-pl-126.local>
2015-12-15 13:50:30 -0700

    1

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SillyApp
+//
+//  Created by Barton, Denon on 12/15/15.
+//  Copyright © 2015 Barton, Denon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBAction func changeColor(sender: UIButton) {
+    }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
